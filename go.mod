765454d96b947e5456fffd514589482d9528429e
Faust Celaj <faustcelaj@mac.home>
2024-11-24 15:27:13 -0500

    added user CRUD operations

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/FaustCelaj/GetFit.git
 
 go 1.23.2
 
-require github.com/gofiber/fiber/v2 v2.52.5
+require (
+	github.com/gofiber/fiber/v2 v2.52.5
+	go.mongodb.org/mongo-driver v1.17.1
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
@@ -21,7 +24,6 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	go.mongodb.org/mongo-driver v1.17.1 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.23.0 // indirect
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -2,8 +2,17 @@ package store
 
 import (
 	"context"
+	"errors"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var (
+	ErrNotFound          = errors.New("resource not found")
+	ErrConflict          = errors.New("resource already exists")
+	QueryTimeoutDuration = time.Second * 5
 )
 
 type Storage struct {
@@ -12,6 +21,9 @@ type Storage struct {
 	}
 	Users interface {
 		Create(context.Context, *User) error
+		GetById(context.Context, primitive.ObjectID) (*User, error)
+		Update(context.Context, primitive.ObjectID, map[string]interface{}) error
+		Delete(context.Context, primitive.ObjectID) error
 	}
 }
 
