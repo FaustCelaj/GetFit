674de38804f90b019098c3a1464eea8a2e6f9f37
Faust Celaj <faustcelaj@mac.home>
2025-01-31 11:44:13 -0500

    react set up

---
+++ web/src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+        <Header>
+            <h1>GetFit</h1>
+            <nav>
+            
+            <ol>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Routines</a></li>
+                <li><a href="#">Login</a></li>
+            </ol>
+        </nav>
+        </Header>
+        
+    )
+}
+
+export default Header
