a425e27a85d0a43ab8511742c68c2de4b4db82d6
Faust <Faustcelaj1@gmail.com>
2025-03-14 13:10:08 -0400

    docs added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,10 +11,32 @@ import (
 
 const version = "0.0.1"
 
+// @title GetFit API
+// @version 0.0.1
+// @description API for tracking workouts, exercises, and fitness routines
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.yoursite.com/support
+// @contact.email your.email@example.com
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:8080
+// @BasePath /api/v1
+//
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @description
+// @schemes http
+
 func main() {
 	// Set up configuration
 	cfg := config{
-		addr: env.GetString("ADDR", ":8080"),
+		addr:   env.GetString("ADDR", ":8080"),
+		apiURL: env.GetString("EXTERNAL_URL", "localHost:8080"),
 		db: dbConfig{
 			addr:         env.GetString("DB_ADDR", "mongodb://localhost:27017"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
